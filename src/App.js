cc765050412fa5696bca4fe6fece5a3adb507759
studyingheesu <heesu.naver@gmail.com>
2021-12-09 11:14:59 +0900

    theme

--- src/App.js
+++ src/App.js
@@ -1,64 +1,22 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const Emoji = styled.span``;
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: ${(props) => props.bgColor};
-  display: flex;
+  background-color: ${(props) => props.theme.backgroundColor};
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-
-  ${Emoji} {
-    font-size: 36px;
-    &:hover {
-      font-size: 60px;
-    }
-  }
 `;
 
-const Circle = styled(Box)`
-  border-radius: 100px;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-const RotatingBox = styled(Box)`
-  animation: ${rotationAnimation} 1s linear infinite;
-`;
-
-const RequiredInput = styled.input.attrs({ required: true, maxLength: 10 })`
-  background-color: lightgray;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <RotatingBox bgColor={'teal'}>
-        <Emoji as="p">😇</Emoji>
-        <Emoji as="p">🥰</Emoji>
-      </RotatingBox>
-      <Emoji as="p">🥸</Emoji>
-      <Circle bgColor={'tomato'} />
-      <Box bgColor={'yellow'} />
-      <RequiredInput type="text" />
-      <RequiredInput type="password" />
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
