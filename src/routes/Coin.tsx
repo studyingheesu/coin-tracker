24669c0d1e59f0fe2b4022807684d7d8b484b23f
studyingheesu <heesu.naver@gmail.com>
2021-12-09 23:01:15 +0900

    pass state through Link

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,47 @@
-import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const LoadingIndicator = styled.span`
+  text-align: center;
+`;
 
 interface RouteParams {
   coinId: string;
 }
+interface RouteState {
+  name: string;
+}
 
 const Coin = () => {
   const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteState>();
+
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || 'Loading..'}</Title>
+      </Header>
+      {loading ? <LoadingIndicator>Loading...</LoadingIndicator> : null}
+    </Container>
+  );
 };
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -80,7 +80,12 @@ const Coins = () => {
         <CoinList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
                 <Icon src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
                 {coin.name} &rarr;
               </Link>
