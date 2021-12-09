67aa565e83f7a41c010a4b63524d7fb43256aa85
studyingheesu <heesu.naver@gmail.com>
2021-12-09 09:52:09 +0900

    add styled-component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import styled, { keyframes } from 'styled-components';
+
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+const Wrapper = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: ${(props) => props.bgColor};
+  animation: ${rotationAnimation} 1s linear infinite;
+`;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Box bgColor={'teal'} />
+    </Wrapper>
   );
 }
 
