516f2d4b91fc3c98bff78cfa5572f421cca461b8
studyingheesu <heesu.naver@gmail.com>
2021-12-09 12:32:18 +0900

    interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  display: flex;
-  background-color: ${(props) => props.theme.backgroundColor};
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import Circle from './Circle';
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <>
+      <Circle bgColor="tomato" />
+      <Circle bgColor="teal" />
+    </>
   );
 }
 
