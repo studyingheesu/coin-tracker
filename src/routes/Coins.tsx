7ff1069e90887e5b7002f0624940a5846ef2acea
studyingheesu <heesu.naver@gmail.com>
2021-12-10 11:34:37 +0900

    apply react-query to Coins

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { fetchCoins } from '../api';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -58,27 +59,18 @@ interface CoinInterface {
 }
 
 const Coins = () => {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { isLoading, data: coins } = useQuery<CoinInterface[]>('coins', fetchCoins);
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('https://api.coinpaprika.com/v1/coins');
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      {loading ? (
+      {isLoading ? (
         <LoadingIndicator>Loading...</LoadingIndicator>
       ) : (
         <CoinList>
-          {coins.map((coin) => (
+          {coins?.slice(0, 100).map((coin) => (
             <Coin key={coin.id}>
               <Link
                 to={{
