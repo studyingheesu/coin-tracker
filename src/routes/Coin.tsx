a24852400753d4b47c5b78777b7088813aee7456
studyingheesu <heesu.naver@gmail.com>
2021-12-10 10:36:04 +0900

    Coin wireframe

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -22,6 +22,28 @@ const LoadingIndicator = styled.span`
   text-align: center;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 interface RouteParams {
   coinId: string;
 }
@@ -101,14 +123,44 @@ const Coin = () => {
 
       setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || 'Loading..'}</Title>
+        <Title>{state?.name ? state.name : loading ? 'Loading...' : info?.name}</Title>
       </Header>
-      {loading ? <LoadingIndicator>Loading...</LoadingIndicator> : null}
+      {loading ? (
+        <LoadingIndicator>Loading...</LoadingIndicator>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? 'Yes' : 'No'}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+        </>
+      )}
     </Container>
   );
 };
